98b80f4ad0c35d7afcb6e52d800d4137d99ac35e
szy <szy@Macbook-Pro.local>
2019-02-09 21:04:16 +0800

    .gitignore

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,7 +2,6 @@ import counter from 'reducers/counter';
 import userInfo from 'reducers/userInfo';
 
 export default function combineReducers(state = {}, action) {
-    console.log(state)
     return {
         counter: counter(state.counter, action),
         userInfo: userInfo(state.userInfo, action)
