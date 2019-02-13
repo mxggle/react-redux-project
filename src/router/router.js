2b3c4b4acd55ad449830a482ec34b6997edf8ca2
mxggle <muggle6594@gmail.com>
2019-02-13 16:23:17 +0800

    升级为16.8，测试新版本hooks

--- src/router/router.js
+++ src/router/router.js
@@ -8,6 +8,10 @@ import Home from 'bundle-loader?lazy&name=home!pages/Home/Home';
 import Page1 from 'bundle-loader?lazy&name=page1!pages/Page1/Page1';
 import Counter from 'bundle-loader?lazy&name=counter!pages/Counter/Counter';
 import UserInfo from 'bundle-loader?lazy&name=userInfo!pages/UserInfo/UserInfo';
+import Hooks from 'bundle-loader?lazy&name=userInfo!pages/Hooks/Hooks';
+import ClassC from 'bundle-loader?lazy&name=userInfo!pages/ClassC/ClassC';
+
+
 
 const Loading = function () {
     return <div>Loading...</div>
@@ -25,16 +29,21 @@ const getRouter = () => (
     <Router>
         <div>
             <ul>
-                <li><Link to="/">首页</Link></li>
+                {/* <li><Link to="/">首页</Link></li>
                 <li><Link to="/page1">Page1</Link></li>
                 <li><Link to="/counter">Counter</Link></li>
-                <li><Link to="/userinfo">UserInfo</Link></li>
+                <li><Link to="/userinfo">UserInfo</Link></li> */}
+                <li><Link to="/Hooks">Hooks</Link></li>
+                <li><Link to="/ClassC">ClassC</Link></li>
+
             </ul>
             <Switch>
                 <Route exact path="/" component={createComponent(Home)}/>
                 <Route path="/page1" component={createComponent(Page1)}/>
                 <Route path="/counter" component={createComponent(Counter)}/>
                 <Route path="/userinfo" component={createComponent(UserInfo)}/>
+                <Route path="/Hooks" component={createComponent(Hooks)}/>
+                <Route path="/ClassC" component={createComponent(ClassC)}/>
             </Switch>
         </div>
     </Router>
